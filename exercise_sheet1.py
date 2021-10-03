b20a8387a230a18eca73a621a17d68e2e8902643
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg>
2021-10-03 16:27:36 +0200

    add programming task

--- exercise_sheet1.py
+++ exercise_sheet1.py
@@ -336,3 +336,25 @@ def exercise_3_c():
     e = None
 
     return a, b, c, d, e
+
+
+########################################################
+############## Programming tasks #######################
+########################################################
+
+from helpers.helpers import translation_table
+
+
+def transcription(dna_string):
+    rna_string = None
+    return rna_string
+
+
+def translation(rna_string):
+    protein = None
+    return protein
+
+
+def central_dogma(dna_seq):
+    protein = None
+    return protein
